e01755326e2e67bc9c4aa28a13dbf2738d93eae3
Kauã <kauanasc2008@gmail.com>
2026-05-20 14:05:46 -0300

    Desenvolvendo Array

---
+++ array/array/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int[] numeros = new int[3]; // Array 3 posições
+
+            // Loop para preencher array
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                // Pede Número (VALOR) 
+                Console.WriteLine("Digite um número: ");
+
+                // Lê valor, converte e armazena na posição do ARRAY
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Mensagem de Separação
+            Console.WriteLine("\nValores digitados: ");
+
+            // Percorre todos elementos do Array
+            foreach (int numero in numeros)
+            {
+                // Exibe cada número armazenado
+                Console.WriteLine(numero);
+            }
+                */
+        }
+    }
+}
